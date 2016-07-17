c319d4fb9d0dad3958be574f7f7efc5d8ba872db
Richard Cook <rcook@rcook.org>
2016-07-16 22:06:23 -0700

    Declare bindings for writer functions

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main (main) where
 
 import Control.Exception
@@ -6,11 +8,9 @@ import Foreign.C
 import Foreign.Ptr
 
 newtype GEOSContextHandle_t = GEOSContextHandle_t (Ptr GEOSContextHandle_t)
-newtype GEOSWKTReaderPtr = GEOSWKTReaderPtr (Ptr GEOSWKTReaderPtr)
 newtype GEOSGeometryPtr = GEOSGeometryPtr (Ptr GEOSGeometryPtr) deriving Show
-
-isNull :: GEOSGeometryPtr -> Bool
-isNull (GEOSGeometryPtr p) = p == nullPtr
+newtype GEOSWKTReaderPtr = GEOSWKTReaderPtr (Ptr GEOSWKTReaderPtr)
+newtype GEOSWKTWriterPtr = GEOSWKTWriterPtr (Ptr GEOSWKTWriterPtr)
 
 foreign import ccall "GEOSversion"
     c_GEOSversion :: CString
@@ -27,10 +27,8 @@ foreign import ccall "helpers.h getErrorMessage"
 foreign import ccall "GEOSGeom_destroy_r"
     c_GEOSGeom_destroy_r :: GEOSContextHandle_t -> GEOSGeometryPtr -> IO ()
 
--- Should probably be in IO
 foreign import ccall "GEOSWKTReader_create_r"
-    c_GEOSWKTReader_create_r :: GEOSContextHandle_t -> GEOSWKTReaderPtr
-
+    c_GEOSWKTReader_create_r :: GEOSContextHandle_t -> IO GEOSWKTReaderPtr
 foreign import ccall "GEOSWKTReader_destroy_r"
     c_GEOSWKTReader_destroy_r :: GEOSContextHandle_t -> GEOSWKTReaderPtr -> IO ()
 
@@ -52,6 +50,13 @@ readGeometry h r wkt =
     let g = c_GEOSWKTReader_read_r h r wkt
     in if isNull g then Nothing else Just g
 
+foreign import ccall "GEOSWKTWriter_create_r"
+    c_GEOSWKTWriter_create_r :: GEOSContextHandle_t -> IO GEOSWKTWriterPtr
+foreign import ccall "GEOSWKTWriter_destroy_r"
+    c_GEOSWKTWriter_destroy_r :: GEOSContextHandle_t -> GEOSWKTWriterPtr -> IO ()
+foreign import ccall "GEOSWKTWriter_write_r"
+    c_GEOSWKTWriter_write_r :: GEOSContextHandle_t -> GEOSWKTWriterPtr -> GEOSGeometryPtr -> IO CString
+
 -- Should this be considered pure?
 foreign import ccall "GEOSIntersection_r"
     c_GEOSIntersection_r ::
@@ -64,8 +69,17 @@ intersection h g0 g1 =
     let g2 = c_GEOSIntersection_r h g0 g1
     in if isNull g2 then Nothing else Just g2
 
+isNull :: GEOSGeometryPtr -> Bool
+isNull (GEOSGeometryPtr p) = p == nullPtr
+
 withGEOS :: (GEOSContextHandle_t -> IO a) -> IO a
 withGEOS = bracket c_initializeGEOSWithHandlers c_uninitializeGEOS
+
+withWKTReader :: GEOSContextHandle_t -> (GEOSWKTReaderPtr -> IO a) -> IO a
+withWKTReader h = bracket (c_GEOSWKTReader_create_r h) (c_GEOSWKTReader_destroy_r h)
+
+withWKTWriter :: GEOSContextHandle_t -> (GEOSWKTWriterPtr -> IO a) -> IO a
+withWKTWriter h = bracket (c_GEOSWKTWriter_create_r h) (c_GEOSWKTWriter_destroy_r h)
 
 main :: IO ()
 main = do
@@ -77,12 +91,19 @@ main = do
     wkt1 <- newCString "POLYGON (( 11 11, 11 12, 12 12, 12 11, 11 11 ))"
 
     void $ withGEOS $ \h -> do
-        let r = c_GEOSWKTReader_create_r h
-            (Just g0) = readGeometry h r wkt0
-            (Just g1) = readGeometry h r wkt1
-            (Just g2) = intersection h g0 g1
+        (g0, g1) <- withWKTReader h $ \reader -> do
+            let !(Just g0) = readGeometry h reader wkt0
+                !(Just g1) = readGeometry h reader wkt1
+            return (g0, g1)
+        let (Just g2) = intersection h g0 g1
         print g0
         print g1
         print g2
-        void $ c_GEOSWKTReader_destroy_r h r
+        withWKTWriter h $ \writer -> do
+            g2cs <- c_GEOSWKTWriter_write_r h writer g2
+            g2Str <- peekCString g2cs
+            print g2Str
+        c_GEOSGeom_destroy_r h g2
+        c_GEOSGeom_destroy_r h g1
+        c_GEOSGeom_destroy_r h g0
     putStrLn "Done"
