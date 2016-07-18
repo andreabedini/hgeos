4bfc50e1095db9093cb1e11f8e26e19a4a164457
Richard Cook <rcook@rcook.org>
2016-07-18 11:31:13 -0700

    Add documentation comments

--- src/lib/Data/Geocoding/GEOS.hs
+++ src/lib/Data/Geocoding/GEOS.hs
@@ -1,7 +1,26 @@
+{-|
+Module      : Data.Geocoding.GEOS
+Description : High-level API for interoperating with GEOS C API
+Copyright   : (C) Richard Cook, 2016
+Licence     : MIT
+Maintainer  : rcook@rcook.org
+Stability   : experimental
+Portability : POSIX
+
+A high-level API for interoperating with GEOS C API which includes automatic
+management of lifetimes of objects such as readers, writers and geometries.
+
+For the low-level FFI bindings, see "Data.Gecoding.GEOS.Imports".
+-}
+
 {-# LANGUAGE RecordWildCards #-}
 
 module Data.Geocoding.GEOS
-    ( intersection
+    ( Context()
+    , Geometry()
+    , Reader()
+    , Writer()
+    , intersection
     , mkReader
     , mkWriter
     , readGeometry
@@ -23,12 +42,16 @@ data ContextState = ContextState
 
 type ContextStateRef = IORef ContextState
 
+-- |Represents a <https://trac.osgeo.org/geos/ GEOS> context
 data Context = Context ContextStateRef
 
+-- |Represents a <https://en.wikipedia.org/wiki/Well-known_text WKT> reader
 data Reader = Reader ContextStateRef GEOSWKTReaderPtr
 
+-- |Represents a <https://en.wikipedia.org/wiki/Well-known_text WKT> writer
 data Writer = Writer ContextStateRef GEOSWKTWriterPtr
 
+-- |Represents a <https://trac.osgeo.org/geos/ GEOS> geometry
 data Geometry = Geometry ContextStateRef GEOSGeometryPtr
 
 mkContext :: IO Context
@@ -45,9 +68,34 @@ releaseContext (Context sr) = do
     mapM_ (c_GEOSWKTReader_destroy_r hCtx) hReaders
     c_uninitializeGEOS hCtx
 
+-- |Creates a <https://trac.osgeo.org/geos/ GEOS> context, passes it to a block
+-- and releases the context and all associated objects such as readers, writers
+-- and geometries at the end:
+--
+-- @
+--    withContext $ \ctx -> do
+--
+--        -- Use context
+--
+--        return ()
+-- @
 withContext :: (Context -> IO a) -> IO a
 withContext = bracket mkContext releaseContext
 
+-- |Creates a reader used to deserialize 'Geometry' instances from
+-- <https://en.wikipedia.org/wiki/Well-known_text WKT>-format text:
+--
+-- @
+--    withContext $ \ctx -> do
+--        reader <- mkReader ctx
+--
+--        -- Use reader
+--
+--        return ()
+-- @
+--
+-- The reader is associated with the 'Context' and released when the 'Context'
+-- is released.
 mkReader :: Context -> IO Reader
 mkReader (Context sr) = do
     ContextState{..} <- readIORef sr
@@ -55,6 +103,20 @@ mkReader (Context sr) = do
     modifyIORef' sr (\p@ContextState{..} -> p { hReaders = hReader : hReaders })
     return $ Reader sr hReader
 
+-- |Creates a writer used to serialize 'Geometry' instances to
+-- <https://en.wikipedia.org/wiki/Well-known_text WKT>-format text:
+--
+-- @
+--    withContext $ \ctx -> do
+--        writer <- mkWriter ctx
+--
+--        -- Use writer
+--
+--        return ()
+-- @
+--
+-- The writer is associated with the 'Context' and released when the 'Context'
+-- is released.
 mkWriter :: Context -> IO Writer
 mkWriter (Context sr) = do
     ContextState{..} <- readIORef sr
@@ -62,6 +124,21 @@ mkWriter (Context sr) = do
     modifyIORef' sr (\p@ContextState{..} -> p { hWriters = hWriter : hWriters })
     return $ Writer sr hWriter
 
+-- |Deserializes a 'Geometry' instance from the given 'String' using the
+-- supplied 'Reader':
+--
+-- @
+--    withContext $ \ctx -> do
+--        reader <- mkReader ctx
+--        geometry <- readGeometry read "POLYGON (( 10 10, 10 20, 20 20, 20 10, 10 10 ))"
+--
+--        -- Use geometry
+--
+--        return ()
+-- @
+--
+-- The geometry is associated with the 'Context' and released when the 'Context'
+-- is released.
 readGeometry :: Reader -> String -> IO Geometry
 readGeometry (Reader sr hReader) str = withCString str $ \cs -> do
     ContextState{..} <- readIORef sr
@@ -69,6 +146,23 @@ readGeometry (Reader sr hReader) str = withCString str $ \cs -> do
     modifyIORef' sr (\p@ContextState{..} -> p { hGeometries = hGeometry : hGeometries })
     return $ Geometry sr hGeometry
 
+-- |Serializes a 'Geometry' instance to a 'String' using the supplied 'Writer':
+--
+-- @
+--    withContext $ \ctx -> do
+--        reader <- mkReader ctx
+--        geometry <- readGeometry read "POLYGON (( 10 10, 10 20, 20 20, 20 10, 10 10 ))"
+--
+--        writer <- mkWriter ctx
+--        str <- writeGeometry writer geometry
+--
+--        -- Use string
+--
+--        return ()
+-- @
+--
+-- The geometry is associated with the 'Context' and released when the 'Context'
+-- is released.
 writeGeometry :: Writer -> Geometry -> IO String
 writeGeometry (Writer sr hWriter) (Geometry _ hGeometry) = do
     ContextState{..} <- readIORef sr
@@ -78,6 +172,24 @@ writeGeometry (Writer sr hWriter) (Geometry _ hGeometry) = do
         peekCString
     return str
 
+-- |Returns the 'Geometry' instance representing the intersection of the two
+-- supplied 'Geometry' instances:
+--
+-- @
+--    withContext $ \ctx -> do
+--        reader <- mkReader ctx
+--        g0 <- readGeometry reader "POLYGON (( 10 10, 10 20, 20 20, 20 10, 10 10 ))"
+--        g1 <- readGeometry reader "POLYGON (( 11 11, 11 12, 12 12, 12 11, 11 11 ))"
+--
+--        g2 <- intersection g0 g1
+--
+--        -- Use geometry
+--
+--        putStrLn str
+-- @
+--
+-- The geometry is associated with the 'Context' and released when the 'Context'
+-- is released.
 intersection :: Geometry -> Geometry -> IO Geometry
 intersection (Geometry sr0 hGeometry0) (Geometry sr1 hGeometry1) = do
     ContextState{..} <- readIORef sr0
--- src/lib/Data/Geocoding/GEOS/Imports.hs
+++ src/lib/Data/Geocoding/GEOS/Imports.hs
@@ -1,44 +1,89 @@
+{-|
+Module      : Data.Geocoding.GEOS.Imports
+Description : FFI bindings for GEOS C API
+Copyright   : (C) Richard Cook, 2016
+Licence     : MIT
+Maintainer  : rcook@rcook.org
+Stability   : experimental
+Portability : POSIX
+
+These are low-level FFI bindings for the GEOS C API derived from
+<http://geos.osgeo.org/doxygen/geos__c_8h_source.html geos_c.h>. These enable
+low-level access to the native functions for parts of the C API for which
+high-level wrappers do not yet exist.
+
+For the high-level API, see "Data.Gecoding.GEOS".
+-}
+
 module Data.Geocoding.GEOS.Imports where
 
 import Foreign.C
 import Foreign.Ptr
 
+-- |Wraps @GEOSContextHandle_t@
 newtype GEOSContextHandle_t = GEOSContextHandle_t (Ptr GEOSContextHandle_t)
+
+-- |Wraps @GEOSGeometry*@
 newtype GEOSGeometryPtr = GEOSGeometryPtr (Ptr GEOSGeometryPtr)
+
+-- |Wraps @GEOSWKTReader*@
 newtype GEOSWKTReaderPtr = GEOSWKTReaderPtr (Ptr GEOSWKTReaderPtr)
+
+-- |Wraps @GEOSWKTWriter*@
 newtype GEOSWKTWriterPtr = GEOSWKTWriterPtr (Ptr GEOSWKTWriterPtr)
 
+-- |Wraps @GEOSversion@
 foreign import ccall "GEOSversion"
     c_GEOSversion :: CString
 
+-- |Wraps @GEOSFree_r@ specialized to @const char*@
 foreign import ccall "GEOSFree_r"
     c_GEOSFree_r_CChar :: GEOSContextHandle_t -> Ptr CChar -> IO ()
 
+-- |Wraps @initializeGEOSWithHandlers@ helper function
 foreign import ccall "helpers.h initializeGEOSWithHandlers"
     c_initializeGEOSWithHandlers :: IO GEOSContextHandle_t
+
+-- |Wraps @uninitializeGEOS@ helper function
 foreign import ccall "helpers.h uninitializeGEOS"
     c_uninitializeGEOS :: GEOSContextHandle_t -> IO ()
+
+-- |Wraps @getNoticeMessage@ helper function
 foreign import ccall "helpers.h getNoticeMessage"
     c_getNoticeMessage :: IO CString
+
+-- |Wraps @getErrorMessage@ helper function
 foreign import ccall "helpers.h getErrorMessage"
     c_getErrorMessage :: IO CString
 
+-- |Wraps @GEOSGeom_destroy_r@
 foreign import ccall "GEOSGeom_destroy_r"
     c_GEOSGeom_destroy_r :: GEOSContextHandle_t -> GEOSGeometryPtr -> IO ()
 
+-- |Wraps @GEOSWKTReader_create_r@
 foreign import ccall "GEOSWKTReader_create_r"
     c_GEOSWKTReader_create_r :: GEOSContextHandle_t -> IO GEOSWKTReaderPtr
+
+-- |Wraps @GEOSWKTReader_destroy_r@
 foreign import ccall "GEOSWKTReader_destroy_r"
     c_GEOSWKTReader_destroy_r :: GEOSContextHandle_t -> GEOSWKTReaderPtr -> IO ()
+
+-- |Wraps @GEOSWKTReader_read_r@
 foreign import ccall "GEOSWKTReader_read_r"
     c_GEOSWKTReader_read_r :: GEOSContextHandle_t -> GEOSWKTReaderPtr -> CString -> IO GEOSGeometryPtr
 
+-- |Wraps @GEOSWKTWriter_create_r@
 foreign import ccall "GEOSWKTWriter_create_r"
     c_GEOSWKTWriter_create_r :: GEOSContextHandle_t -> IO GEOSWKTWriterPtr
+
+-- |Wraps @GEOSWKTWriter_destroy_r@
 foreign import ccall "GEOSWKTWriter_destroy_r"
     c_GEOSWKTWriter_destroy_r :: GEOSContextHandle_t -> GEOSWKTWriterPtr -> IO ()
+
+-- |Wraps @GEOSWKTWriter_write_r@
 foreign import ccall "GEOSWKTWriter_write_r"
     c_GEOSWKTWriter_write_r :: GEOSContextHandle_t -> GEOSWKTWriterPtr -> GEOSGeometryPtr -> IO CString
 
+-- |Wraps @GEOSIntersection_r@
 foreign import ccall "GEOSIntersection_r"
     c_GEOSIntersection_r :: GEOSContextHandle_t -> GEOSGeometryPtr -> GEOSGeometryPtr -> IO GEOSGeometryPtr
