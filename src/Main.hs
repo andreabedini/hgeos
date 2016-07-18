10314faf534080069c4e9a8d297f5aa0a579851f
Richard Cook <rcook@rcook.org>
2016-07-17 22:23:24 -0700

    New high-level API

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,96 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
+import Control.Exception
+import Data.IORef
 import Foreign.C
 import Data.Geocoding.GEOS.HighLevelAPI
 import Data.Geocoding.GEOS.LowLevelAPI
+
+newtype Handle = Handle Int
+
+data ContextState = ContextState
+    { hCtx :: GEOSContextHandle_t
+    , hReaders :: [GEOSWKTReaderPtr]
+    , hWriters :: [GEOSWKTWriterPtr]
+    , hGeometries :: [GEOSGeometryPtr]
+    }
+
+type ContextStateRef = IORef ContextState
+
+data Context = Context ContextStateRef
+
+data Reader = Reader ContextStateRef GEOSWKTReaderPtr
+
+data Writer = Writer ContextStateRef GEOSWKTWriterPtr
+
+data Geometry = Geometry ContextStateRef GEOSGeometryPtr
+
+mkContext :: IO Context
+mkContext = do
+    hCtx <- c_initializeGEOSWithHandlers
+    sr <- newIORef $ ContextState hCtx [] [] []
+    return $ Context sr
+
+releaseContext :: Context -> IO ()
+releaseContext (Context sr) = do
+    ContextState{..} <- readIORef sr
+    mapM_ (c_GEOSGeom_destroy_r hCtx) hGeometries
+    mapM_ (c_GEOSWKTWriter_destroy_r hCtx) hWriters
+    mapM_ (c_GEOSWKTReader_destroy_r hCtx) hReaders
+    c_uninitializeGEOS hCtx
+
+mkReader__ :: Context -> IO Reader
+mkReader__ (Context sr) = do
+    ContextState{..} <- readIORef sr
+    hReader <- c_GEOSWKTReader_create_r hCtx
+    modifyIORef' sr (\p@ContextState{..} -> p { hReaders = hReader : hReaders })
+    return $ Reader sr hReader
+
+mkWriter__ :: Context -> IO Writer
+mkWriter__ (Context sr) = do
+    ContextState{..} <- readIORef sr
+    hWriter <- c_GEOSWKTWriter_create_r hCtx
+    modifyIORef' sr (\p@ContextState{..} -> p { hWriters = hWriter : hWriters })
+    return $ Writer sr hWriter
+
+readGeometry__ :: Reader -> String -> IO Geometry
+readGeometry__ (Reader sr hReader) str = withCString str $ \cs -> do
+    ContextState{..} <- readIORef sr
+    hGeometry <- c_GEOSWKTReader_read_r hCtx hReader cs
+    modifyIORef' sr (\p@ContextState{..} -> p { hGeometries = hGeometry : hGeometries })
+    return $ Geometry sr hGeometry
+
+writeGeometry__ :: Writer -> Geometry -> IO String
+writeGeometry__ (Writer sr hWriter) (Geometry _ hGeometry) = do
+    ContextState{..} <- readIORef sr
+    cs <- c_GEOSWKTWriter_write_r hCtx hWriter hGeometry
+    str <- peekCString cs
+    c_GEOSFree_r_CChar hCtx cs
+    return str
+
+intersection__ :: Geometry -> Geometry -> IO Geometry
+intersection__ (Geometry sr0 hGeometry0) (Geometry sr1 hGeometry1) = do
+    ContextState{..} <- readIORef sr0
+    hGeometry <- c_GEOSIntersection_r hCtx hGeometry0 hGeometry1
+    return $ Geometry sr0 hGeometry
+
+withContext__ :: (Context -> IO a) -> IO a
+withContext__ = bracket mkContext releaseContext
+
+newAPIDemo :: IO ()
+newAPIDemo = do
+    withContext__ $ \ctx -> do
+        reader <- mkReader__ ctx
+        g0 <- readGeometry__ reader "POLYGON (( 10 10, 10 20, 20 20, 20 10, 10 10 ))"
+        g1 <- readGeometry__ reader "POLYGON (( 11 11, 11 12, 12 12, 12 11, 11 11 ))"
+        g2 <- intersection__ g0 g1
+        writer <- mkWriter__ ctx
+        str <- writeGeometry__ writer g2
+        putStrLn str
+        putStrLn "Done"
 
 lowLevelAPIDemo :: IO ()
 lowLevelAPIDemo = do
@@ -49,3 +135,4 @@ main = do
     putStrLn s
     lowLevelAPIDemo
     highLevelAPIDemo
+    newAPIDemo
